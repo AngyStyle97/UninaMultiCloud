cdc035f5f7faa322a5e4c3b3f5e194e3fd622336
Angelo De Fenza <angystyle97@outlook.it>
2026-07-06 19:26:51 +0200

    Aggiunta classe utente

---
+++ src/entity/Utente.java
@@ -0,0 +1,78 @@
+package entity;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Utente {
+
+	private String email;
+	private String nome;
+	private String cognome;
+	private String password;
+	private Date dataIscrizione;
+	private int numeroCaricamenti;
+	private Date dataNascita;
+	private ArrayList<Playlist> playlist;
+	
+	public Utente(String email, String nome, String cognome, String password, Date dataIscrizione,
+			int numeroCaricamenti, Date dataNascita) {
+		
+		this.email = email;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.password = password;
+		this.dataIscrizione = dataIscrizione;
+		this.dataNascita = dataNascita;
+		this.numeroCaricamenti = numeroCaricamenti;
+		this.playlist = new ArrayList<>();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public Date getDataIscrizione() {
+		return dataIscrizione;
+	}
+
+	public int getNumeroCaricamenti() {
+		return numeroCaricamenti;
+	}
+
+	public Date getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void addPlaylist(Playlist p) {
+		playlist.add(p);
+	}
+	
+	public void remPlaylist(Playlist p) {
+		playlist.remove(p);
+	}
+	
+}
