772b2817e67600c7bb9aa57f0ec8dd7e8ac5fbeb
Gennaro <bitondogennaro@gmail.com>
2026-07-07 12:29:34 +0200

    semplice classe astratta interfaces

--- src/dao/UtenteDAO.java
+++ src/dao/UtenteDAO.java
@@ -1,5 +1,29 @@
 package dao;
 
+import entity.Utente;
+
 public interface UtenteDAO {
+	
+	
+	boolean SalvaUtente(Utente u);
+	
+	Utente CercaEmail(String email);
+	
+	String AggiornaUtente(Utente u);
+	
+	boolean EliminaUtente(String email);
+	
+	
+	
+	
+	
 
 }
+  
+	
+	
+	
+
+
+	 
+	
