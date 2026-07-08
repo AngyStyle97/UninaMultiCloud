aff7c3bb5cfabbd77c683ca91b14185d5c37ea58
Gennaro <bitondogennaro@gmail.com>
2026-07-08 11:21:19 +0200

    PLAYLIST DAO

---
+++ src/dao/PlaylistDAO.java
@@ -0,0 +1,20 @@
+package dao;
+
+import entity.Playlist;
+import java.util.ArrayList;
+
+public interface PlaylistDAO {
+	
+	boolean salvaPlaylist(Playlist p);
+	
+	Playlist cercaPerId(String idPlaylist);
+	
+	boolean aggiornaPlaylist(Playlist p);
+	
+	boolean  eliminaPlaylist(String idPlaylist);
+	
+	ArrayList<Playlist> cercaPerUtente(String email);
+	
+	ArrayList<Playlist> cercaPerTipo (String tipoPlaylist); 
+
+}
