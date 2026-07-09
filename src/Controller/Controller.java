0799608cf4178759980ed93b61f97eb00b0be391
Gennaro <bitondogennaro@gmail.com>
2026-07-09 13:16:04 +0200

    BOZZA di gui e di controller (ERRORI)

---
+++ src/Controller/Controller.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import dao.UtenteDAO;
+import dao.jdbc.JDBCUtenteDAO;
+import entity.Utente;
+
+public class Controller {
+ 
+    private UtenteDAO utenteDAO = new JDBCUtenteDAO();
+    
+    
+    public Utente effettuaLogin(String email, String password) {
+        return utenteDAO.login(email, password);
+    }
+}
